a8f7e5c96050248624c2c906168decba4805beda
Giyos1 <giyosoripov4@gmail.com>
2022-08-10 16:33:53 +0500

    hacbsja

--- users/models.py
+++ users/models.py
@@ -9,7 +9,13 @@ class Sanalar(models.Model):
     from_id = models.TextField(null=False)
     to_id = models.TextField(null=False)
 
+    class Meta:
+        db_table = 'sanalar'
+
 
 class Admins(models.Model):
     admin_id = models.TextField()
     group_id = models.TextField()
+
+    class Meta:
+        db_table = 'admins'
