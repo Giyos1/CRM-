0b94c111fe9220e46ca9129a506f2eaf9b01411b
Giyos1 <giyosoripov4@gmail.com>
2022-08-10 16:19:15 +0500

    add table

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Sanalar(models.Model):
+    dars = models.IntegerField(null=False)
+    sana = models.TextField(null=False)
+    from_id = models.TextField(null=False)
+    to_id = models.TextField(null=False)
+
+
+class Admins(models.Model):
+    admin_id = models.TextField()
+    group_id = models.TextField()
